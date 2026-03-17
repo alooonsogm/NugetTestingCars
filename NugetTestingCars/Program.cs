1806546f052a0cd23af1b6bcbbb916626f6e55fb
alooonsogm <alonsogarciamartin2@gmail.com>
2026-03-17 14:13:39 +0100

    Add project files.

---
+++ NugetTestingCars/Program.cs
@@ -0,0 +1,10 @@
+﻿using NugetCarsAGM;
+
+Console.WriteLine("Mis coches del NuGet:");
+Garaje g = new Garaje();
+List<Coche> coches = g.GetCoches();
+foreach (Coche c in coches)
+{
+    Console.WriteLine(c.Marca + " " + c.Modelo);
+}
+Console.WriteLine("Fin del programa");
